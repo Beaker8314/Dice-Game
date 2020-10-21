ed266732fc153454481b2566c14ec96a27f831d3
Beaker8314 <49918132+Beaker8314@users.noreply.github.com>
2020-10-20 23:04:52 -0400

    added files for dice game

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random number 1-6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[0]; // first img element
+image1.setAttribute("src", randomImageSource); //changes the src attribute to a random dice image using radnomImageSource
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //random number 1-6
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2); //second img element change src to random dice image
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
